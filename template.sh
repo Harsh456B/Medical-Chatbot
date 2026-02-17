57d7f7e829ee305468a07f5d40272c2a5fd8c508
Harsh Bhogal <harshbhogal1@gmail.com>
2026-02-17 09:02:19 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+mkdir -p research
+mkdir -p src
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+touch research/trials.ipynb
+
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+
+echo "successfully created"
